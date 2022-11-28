06b58e9244b68cd3f49533378739fd434766a7c1
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-11-28 10:36:36 +0000

    Compose logic to display books data as a logical function

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
-const booksData = [];
+/* eslint-disable linebreak-style */
+import bookCardBuilder from './bookCardBuilder';
+import { getBooksDataFromLocalStorage } from './utils';
+
 const addButton = document.querySelector('#add-data');
 const cardsDiv = document.querySelector('.book-author-collections');
 const getLocalst = JSON.parse(localStorage.getItem('books'));
 
-if(getLocalst == undefined) {
-    cardsDiv.innerHTML = `<div></div>`
-} else {
-    for(let i=0; i<getLocalst.length; i++){
-        cardsDiv.innerHTML += `
-    <div class="book-author--card">
-          <p>${getLocalst[i].Title}</p>
-          <p>${getLocalst[i].Author}</p>
-          <button type="button" data-remove>Remove</button>
-        </div>
-    `
-    }
-}
+const booksData = [];
 
+const displayData = () => {
+  const booksData = getBooksDataFromLocalStorage();
+  booksData.forEach((book) => {
+    const bookCard = bookCardBuilder(book);
+    cardsDiv.innerHTML += bookCard;
+  });
+};
+
+
+
+// if(getLocalst == undefined) {
+//     cardsDiv.innerHTML = `<div></div>`
+// } else {
+//     for(let i=0; i<getLocalst.length; i++){
+//         cardsDiv.innerHTML += `
+//     <div class="book-author--card">
+//           <p>${getLocalst[i].Title}</p>
+//           <p>${getLocalst[i].Author}</p>
+//           <button type="button" data-remove>Remove</button>
+//         </div>
+//     `
+//     }
+// }
 
 
 // add-function
-
 addButton.addEventListener('click', () => {
     const title = document.querySelector('#book-title');
     const author = document.querySelector('#author-name');
