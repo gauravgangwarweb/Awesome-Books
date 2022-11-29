b7e679212c353a03b376237b4899e02eb64c4a94
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-11-29 13:53:26 +0000

    Implement basic add and remove methods in Book class

--- js/Book.js
+++ js/Book.js
@@ -4,6 +4,15 @@ class Book {
     this.bookTitle = bookTitle;
     this.bookAuthor = bookAuthor;
   }
+
+  static addBookToStorage(bookDetails) {
+    const { bookTitle, bookAuthor } = bookDetails;
+    console.log('Adding books to storage', bookTitle, bookAuthor);
+  }
+
+  static removeBookFromStorage(bookId) {
+    console.log(`Book with id ${bookId} removed from local storage`);
+  }
 }
 
 export default Book;
