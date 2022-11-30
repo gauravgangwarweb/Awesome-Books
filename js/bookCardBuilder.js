357774a4fe45f0a04601a03857d355f94b938535
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-11-30 06:28:17 +0000

    Refactor markup for card builder

--- js/bookCardBuilder.js
+++ js/bookCardBuilder.js
@@ -2,10 +2,11 @@
 const bookCardBuilder = (data) => {
   const { bookId, bookTitle, bookAuthor } = data;
   const card = `
-      <div class="book-author--card" id="${bookId}">
-        <p>${bookTitle}</p>
-        <p>${bookAuthor}</p>
-        <button type="button"  data-remove-btn>Remove</button>
+      <div class="book-author--card row" id="${bookId}">
+        <div>
+          <p>${bookTitle} by ${bookAuthor}</p>
+        </div>
+        <button type="button" class="btn btn-remove"  data-remove-btn>Remove</button>
       </div>
   `;
   return card;
